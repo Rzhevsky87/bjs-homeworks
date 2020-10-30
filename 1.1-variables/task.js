a8b715a0634d8528614dd2db44fc0913b654aa55
Rzhevsky87 <egorbarisovi4@gmail.com>
2020-10-30 16:35:39 +0500

    task -1

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,34 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	// реализуйте здесь задание №1
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	
+	let result = (algebra + geography + physics) / 3;
+
+	return result;
 }
 
 function sayHello(userName) {
 	
     // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	
+	let message = `Привет, мир! Меня зовут ${myName}`;
+
+	return message;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	
+	let x, y, z;
+	x = 2;
+	y = 22;
+	z = 0;
+
+    return (x * y + 5 * z + x - 1);
 }
